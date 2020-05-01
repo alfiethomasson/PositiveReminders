4bb6904ec0b8c738f74fab8d3d2e10bcdba9aeba
Alfie Thomasson <alfiethomasson0@gmail.com>
2020-05-02 00:25:33 +0100

    First Commit

---
+++ PositiveReminder/PositiveReminder/MessageHolder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PositiveReminder
+{
+    public class MessageHolder
+    {
+        public static List<string> messages;
+        public static void Start()
+        {
+            messages = new List<string>();
+            messages.Add("You are Appreciated");
+            messages.Add("You are popular");
+            messages.Add("You are awesome");
+            messages.Add("You are useful");
+        }
+
+        public static string GetRandomMessage()
+        {
+            int random = RandomNumber(0, messages.Count);
+            return messages[random];
+        }
+
+        public static int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+    }
+}
